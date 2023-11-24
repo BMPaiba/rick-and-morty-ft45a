1c8f083811bcee8948a9288223071a875a69d5cc
Brian Paiba <brianmanuelpaiba@outlook.es>
2023-11-24 19:02:37 -0300

    info

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ export default function About(props) {
         <h1>About me</h1>
         <p>Visita mi 
           <a
-            href="https://github.com/HX-ARomero"
+            href="https://github.com/BMPaiba"
             target="_blank"
           > GitHub</a>
         </p>
