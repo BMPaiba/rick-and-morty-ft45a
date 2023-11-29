d3d6fd255b69a97208d39f8f33242af100124887
Brian Paiba <brianmanuelpaiba@outlook.es>
2023-11-29 19:21:52 -0300

    cambios en el server

--- server/src/index.js
+++ server/src/index.js
@@ -2,27 +2,15 @@ const http = require("http");
 const { listeners } = require("process");
 const PORT = 3001;
 const characters = require("./utils/data");
+const {getCharById} = require("./controllers/getCharById");
 
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     if (req.url.includes("/rickandmorty/character")) {
-      const id = req.url.split("/").pop();
-      const character = characters.find((char) => char.id === Number(id)); //find devuelve el elemento, filter, un array con el elemento
-      // console.log(characters);
-      if (character) {
-        return res
-          .writeHead(200, { "Content-Type": "application/json" })
-          .end(JSON.stringify(character));
-      } else {
-        return res
-          .writeHead(404, { "Content-Type": "application/json" })
-          .end(JSON.stringify({ message: "Character Noy Found" }));
-      }
+      const id = Number(req.url.split("/").pop());
+      getCharById(res, id);
     }
-    return res
-      .writeHead(404, { "Content-Type": "application/json" })
-      .end(JSON.stringify({ message: "Wrong URL" }));
   })
   .listen(PORT, "127.0.0.1", () => {
     console.log(`Server running on port ${PORT}`);
